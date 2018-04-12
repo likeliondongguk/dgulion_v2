bf7061fb409153e1c99ff90b50fbd96c63339af6
wonno <jongwonno@likelion.org>
2018-04-12 20:08:53 +0900

    wonno bug fix

--- app/admin/ecomments.rb
+++ app/admin/ecomments.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Ecomment do
   menu parent: "대나무숲관리"
+  permit_params :body, :secret
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
